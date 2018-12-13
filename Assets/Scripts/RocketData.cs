1a4a65d37b533ac3c7089969f95768ea8a404981
Jeremy Stevens <jeremy.stevens@bglgroup.co.uk>
2018-12-13 12:17:05 +0000

    created RocketData

---
+++ Assets/Scripts/RocketData.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketData : ScriptableObject {
+    public int launchCost;
+    public int power;
+}
