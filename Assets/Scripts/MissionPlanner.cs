615e53edba1e2567ad11d165c5ddff4d4a2dbcb2
Jeremy Stevens <jeremy.stevens@bglgroup.co.uk>
2018-12-17 11:00:41 +0000

    stubbed out mission planner class to populate the ui

---
+++ Assets/Scripts/MissionPlanner.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MissionPlanner : MonoBehaviour {
+
+	public RectTransform rocketList;
+	public RectTransform payloadList;
+	public RectTransform destinationList;
+	public Registry registry;
+	public ListItemUI listItem;
+
+	void Start() {
+		foreach (var rocket in registry.rockets) {
+			addItem(rocketList, rocket.name, "Power: " + rocket.power, "Cost: " + rocket.launchCost, null);
+		}
+		foreach (var payload in registry.payloads) {
+			addItem(payloadList, payload.name, "Weight: " + payload.weight, "Value: " + payload.launchValue, "Bonus: " + payload.successBonus);
+		}
+		foreach (var destination in registry.destinations) {
+			addItem(destinationList, destination.name, "Power: " + destination.requiredPower, "Duration: " + destination.baseMissionDuration, null);
+		}
+	}
+
+	private void addItem(RectTransform parent, string name, string one, string two, string three) {
+		ListItemUI item = Instantiate(listItem);
+		item.transform.SetParent(parent, false);
+		item.setData(name, one, two, three);
+	}
+}
